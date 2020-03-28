7983b2ba574b54822984e7d85fae1ba00e66907b
j.h.brandt@student.utwente.nl <djekkoo@hotmail.com>
2020-03-28 22:13:12 +0100

    added dropdown selector for required number of parts necessary for reconstruction

--- app/src/main/java/com/example/test/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/test/ui/dashboard/DashboardFragment.kt
@@ -17,7 +17,8 @@ class DashboardFragment : Fragment() {
     private lateinit var dashboardViewModel: DashboardViewModel
     private lateinit var root : View
 
-    private val defaultPartsNumberValue: String = "5";
+    private val defaultPartsNumberValue: String = "5"
+    private val defaultReconstructionPartsNumberValue: String = "3"
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -36,7 +37,7 @@ class DashboardFragment : Fragment() {
         val calculatePartsButton : Button = this.root.findViewById(R.id.calculate_parts_button)
         calculatePartsButton.setOnClickListener { calculate_parts() }
 
-        // dropdown spinner
+        // dropdown spinner: total number of parts
         val numberPartsSpinner : Spinner = this.root.findViewById(R.id.spinner_number_parts)
         val spinnerAdapter = ArrayAdapter.createFromResource(
             this.activity!!,
@@ -48,6 +49,19 @@ class DashboardFragment : Fragment() {
         }
         // set default value
         numberPartsSpinner.setSelection(spinnerAdapter.getPosition(this.defaultPartsNumberValue))
+
+        // dropdown spinner: parts necessary for reconstruction
+        val numberReconstructionPartsSpinner : Spinner = this.root.findViewById(R.id.spinner_number_reconstruct_parts)
+        val reconstructionSpinnerAdapter = ArrayAdapter.createFromResource(
+            this.activity!!,
+            R.array.reconstruct_parts_number_selection,
+            android.R.layout.simple_spinner_dropdown_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            numberReconstructionPartsSpinner.adapter = adapter
+        }
+        // set default value
+        numberReconstructionPartsSpinner.setSelection(reconstructionSpinnerAdapter.getPosition(this.defaultReconstructionPartsNumberValue))
 
         return this.root
     }
